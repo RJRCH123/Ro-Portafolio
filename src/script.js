5cfd402e133e529821bc07b2d8dfe12b82827506
Rosamaria Rodriguez <rous9716@gmail.com>
2022-03-09 17:18:42 -0500

    Fix: Resolve conflicts in qualifications scripts

--- src/script.js
+++ src/script.js
@@ -51,7 +51,7 @@ skillsHeader.forEach((el) => {
 const tabs = document.querySelectorAll('[data-target]'),
       tabContents = document.querySelectorAll('[data-content]')
 
-tabs.forEach(tab => {
+tabs.forEach(tab =>{
   tab.addEventListener('click', () =>{
     const target = document.querySelector(tab.dataset.target)
 
@@ -60,7 +60,7 @@ tabs.forEach(tab => {
     })
     target.classList.add('qualification__active')
 
-    tab.forEach(tab =>{
+    tabs.forEach(tab =>{
       tab.classList.remove('qualification__active')
     })
     tab.classList.add('qualification__active')
