e80e2fd2bcf1a6a911cfd2aacaffc03ed2c77ba7
Rosamaria Rodriguez <rous9716@gmail.com>
2022-03-10 02:39:54 -0500

    Feat: Add testimonials section

--- src/script.js
+++ src/script.js
@@ -91,7 +91,7 @@ modalCloses.forEach((modalClose) =>{
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper(".portfolio__container", {
+let swiperPortfolio = new Swiper(".portfolio__container", {
   cssMode: true,
   loop:true,
   navigation: {
@@ -105,7 +105,23 @@ let swiper = new Swiper(".portfolio__container", {
 });
 
 /*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper(".testimonial__container", {
+  loop:true,
+  grabCursor: true,
+  spaceBetween: 48,
 
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  breakpoints: {
+    568: {
+      slidesPerView:2,
+    }
+  }
+});
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
