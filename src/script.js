a1edb69e047cca8a2c38393cddac32a09a2f1bf7
Rosamaria Rodriguez <rous9716@gmail.com>
2022-03-12 02:04:09 -0500

    Fix: Add filters for projects

--- src/script.js
+++ src/script.js
@@ -123,6 +123,27 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
   }
 });
 
+/*==================== MIXITUP FILTER PORTFOLIO ====================*/ 
+let mixerPortfolio = mixitup('.work__container', {
+  selectors: {
+      target: '.work__card'
+  },
+  animation: {
+      duration: 300
+  }
+});
+
+/* Link active work */
+const linkWork = document.querySelectorAll('.work__item')
+
+function activeWork(){
+  linkWork.forEach(L => L.classList.remove('active-work'))
+  this.classList.add('active-work')
+}
+
+linkWork.forEach(L => L.addEventListener('click', activeWork))
+
+
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
